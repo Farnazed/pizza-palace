c786d220484a37ed137992cf0ede9f576b5ebd0d
farnazed <farnaz.e.daneshvar@gmail.com>
2021-01-06 14:59:04 -0700

    procfile added

--- app.js
+++ app.js
@@ -9,16 +9,16 @@ app.use(express.json());
 
 app.use('/api', api);
 
-const port = process.env.PORT || 5000;
-app.listen(port, function () {
-  console.log('Server running on port 5000');
-});
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './frontend/build')));
   app.get('*', (req, res) =>
     res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
   );
 }
+
+const port = process.env.PORT || 5000;
+app.listen(port, function () {
+  console.log('Server running on port 5000');
+});
 
 module.exports = app;
